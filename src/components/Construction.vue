c994000f806f074cabc0a38c63278fc89ce9942a
Jason Byer <jbyer3@gmail.com>
2019-03-04 12:17:51 -0500

    construction update

--- src/components/Construction.vue
+++ src/components/Construction.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
+  <div class="content">
     <h1>Anne Pengilly</h1>
     <h3>Collaborative Pianist</h3>
     <h4>Site coming soon.</h4>
+    <p>For Inquiries please contact</p>
+    <ul>
+      <li><strong>phone:</strong> (704) 247.7965</li>
+      <li><strong>email:</strong> pengillyplayspiano@gmail.com</li>
+    </ul>
   </div>
 </template>
 
@@ -12,6 +17,19 @@ html, body {
   margin: 0;
   box-sizing: border-box;
   background: #dad8f3;
+  margin: 0 auto;
+}
+.content{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  // width: 38vw;
+}
+ul {
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 }
 </style>
 
