88c1e01358aa362146a3270379d0a4bdb7cd610e
Jason Byer <jbyer3@gmail.com>
2019-03-06 17:10:52 -0500

    drinking

--- src/components/CallToAction.vue
+++ src/components/CallToAction.vue
@@ -4,9 +4,7 @@
       <h1>Contact me for a gig</h1>
       <form action="" name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
         <input type="hidden" name="form-name" value="contact" />
-        <p class="hidden">
-          <label>dont fill this out: <input name="bot-field"/></label>
-        </p>
+        <input type="hidden" name="bot-field"/>
         <label for="name">name:</label>
         <input class="text-inputs" type="text" name="name" id="name" required>
         <label for="email">email:</label>
